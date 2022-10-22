bd7f50ec5c2dd235a2f58d37b46296e13fe83a4a
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2022-10-22 12:38:30 +0200

    changed the baseUri based on the node environment

--- src/api/consts.js
+++ src/api/consts.js
@@ -1,1 +1,4 @@
-export const baseUri = "http://localhost:4000/";
+export const baseUri =
+  process.env.NODE_ENV === "production"
+    ? "https://sleepy-waters-11578.herokuapp.com/"
+    : "http:localhost:4000";
